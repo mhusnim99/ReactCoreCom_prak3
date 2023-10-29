1ff8445f0b69b2557df58c6f3cd42cbdbac3df3b
Muhammad Husni Mubarrok <mubarrokhusni12@gmail.com>
2023-10-29 15:28:36 +0700

    recode

--- components/separator.js
+++ components/separator.js
@@ -1,8 +1,7 @@
-import { View } from "react-native";
-
+// import { View } from "react-native";
+import { View } from "@gluestack-ui/themed";
 // Functional Component with props
 const Separator = (props) => {
-  return <View style={{ height: props.height }}></View>;
+return <View style={{ height: props.height }}></View>;
 };
-
 export default Separator;
